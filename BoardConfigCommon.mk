95d8f88fe9fcad506e1f2ed5032c8cd2528d6631
Chenyang Zhong <zhongcy95@gmail.com>
2022-05-04 08:42:17 -0400

    sm8450-common: enable Dolby Vision in hwcomposer
    Signed-off-by: Chenyang Zhong <zhongcy95@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -65,6 +65,11 @@ TARGET_CAMERA_OVERRIDE_FORMAT_FROM_RESERVED := true
 DEX2OAT_TARGET_CPU_VARIANT := cortex-a76
 DEX2OAT_TARGET_CPU_VARIANT_RUNTIME := cortex-a76
 
+# Dolby Vision
+SOONG_CONFIG_NAMESPACES += dolby_vision
+SOONG_CONFIG_dolby_vision += enabled
+SOONG_CONFIG_dolby_vision_enabled := true
+
 # Filesystem
 TARGET_FS_CONFIG_GEN := $(COMMON_PATH)/configs/config.fs
 
