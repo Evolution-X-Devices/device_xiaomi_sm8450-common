4e5d033dad711c02deb78e70f23e1110c065e3bb
johnmart19 <johnivan19999@gmail.com>
2022-09-01 03:10:20 +0300

    sm8450-common: BT: Import `LDAC and LHDC` prebuilts
    Signed-off-by: SharmagRit <sharmagritstuff@gmail.com>

--- common.mk
+++ common.mk
@@ -112,6 +112,17 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.bluetooth.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth.xml \
     frameworks/native/data/etc/android.hardware.bluetooth_le.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth_le.xml
 
+# Bluetooth Library Deps
+ PRODUCT_PACKAGES += \
+     libbluetooth_audio_session \
+     libbthost_if.vendor \
+     libldacBT_bco \
+     libldacBT_bco.vendor \
+     liblhdc \
+     liblhdcBT_enc \
+     liblhdcdec \
+     liblhdcBT_dec
+
 # Boot control
 PRODUCT_PACKAGES += \
     android.hardware.boot-service.qti \
