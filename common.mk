74f35ed6f64b863adc4bcc3bd1093c211c7af5bc
Edwin Moquete <edwinmmoquete@gmail.com>
2024-03-16 01:24:14 +0000

    sm8450-common: Build android.media.audio.common.types-V2-cpp
    Needed for WFD, and not built default on QPR2
    
    Change-Id: If860613ef4cb6bae7f7a7e72707c24510e36abd6

--- common.mk
+++ common.mk
@@ -519,6 +519,7 @@ PRODUCT_COPY_FILES += \
 
 # WiFi Display
 PRODUCT_PACKAGES += \
+    android.media.audio.common.types-V2-cpp:64 \
     vendor.qti.hardware.display.config-V5-ndk:64
 
 # WiFi firmware symlinks
