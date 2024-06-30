a775c09706931a3ac3d5e18a456cf32ea8ba186f
Joey <joey@evolution-x.org>
2024-07-01 02:33:03 +0900

    [TMP] Revert "sm8450-common: parts: Disable NFC polling during front camera use"
    This reverts commit e8bc6e36271ec794a0472086ba55e06221e67cba.

--- parts/src/org/lineageos/settings/BootCompletedReceiver.java
+++ parts/src/org/lineageos/settings/BootCompletedReceiver.java
@@ -22,7 +22,6 @@ import android.content.Context;
 import android.content.Intent;
 import android.util.Log;
 
-import org.lineageos.settings.camera.NfcCameraService;
 import org.lineageos.settings.doze.DozeUtils;
 import org.lineageos.settings.thermal.ThermalUtils;
 import org.lineageos.settings.refreshrate.RefreshUtils;
@@ -40,7 +39,6 @@ public class BootCompletedReceiver extends BroadcastReceiver {
 
         Log.i(TAG, "Boot completed, starting services");
         DozeUtils.onBootCompleted(context);
-        NfcCameraService.startService(context);
         RefreshUtils.startService(context);
         ThermalUtils.startService(context);
     }
