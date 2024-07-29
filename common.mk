5c973e6a79d8e398af0314b41908c392719e02e3
Cosmin Tanislav <demonsingur@gmail.com>
2024-07-29 23:34:33 +0300

    sm8450-common: build libstagefright_softomx_plugin
    vndksupport: Could not load libstagefright_softomx_plugin.so from default namespace: dlopen failed: library "libstagefright_softomx_plugin.so" not found
    
    Change-Id: I56b620ebb9f9dc64ecc492bdb7ee17c3df57e593

--- common.mk
+++ common.mk
@@ -240,7 +240,8 @@ PRODUCT_PACKAGES += \
     init.qti.media.rc \
     init.qti.media.sh \
     libOmxCore \
-    libstagefrighthw
+    libstagefrighthw \
+    libstagefright_softomx_plugin.vendor
 
 PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_audio.xml \
