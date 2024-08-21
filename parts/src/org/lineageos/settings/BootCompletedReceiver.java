89e7e1e8fdf3c778cd5253e1aeacdb85a241fcb6
kenway214 <kenway214@outlook.com>
2024-08-21 22:57:03 +0530

    sm8450-common: parts: Introduce Display Saturation Service and TileUtils
    * Adjust Saturation Value Handling for Device Color Mode Compatibility
    
    This update refines the saturation adjustment mechanism. When the seek bar progress is set, the method executes
    the command "service call SurfaceFlinger 1022 f [saturation]" via Runtime.getRuntime().exec() to apply the saturation changes.
    However, it was observed that when the device's color mode is set to "saturated," a saturation value of 1.0 does not trigger any visual changes.
    To address this, the code now sets a value of 1.001 for a seek bar value of 100, ensuring the desired effect is consistently applied.
    
    * TileUtils Implementation: Implemented TileUtils to handle the addition of Quick Settings tiles, specifically for the saturation feature.
    This is done with reference to the guidelines provided by Google
    https://developer.android.com/develop/ui/views/quicksettings-tiles#prompt-user
    this ensuring a seamless integration and user experience when creating new tiles.
    
    Signed-off-by: Pabloescobar-reborn <pabloescobarreborn77@gmail.com>
    Co-Authored-By: kenway214 <kenway214@outlook.com>
    Co-Authored-By: AnierinB <anierin@evolution-x.org>
    Co-Authored-By: SKULSHADY <anushekprasal@gmail.com>
    Co-Authored-By: jhenrique09 <jhenrique09.mcz@hotmail.com>

--- parts/src/org/lineageos/settings/BootCompletedReceiver.java
+++ parts/src/org/lineageos/settings/BootCompletedReceiver.java
@@ -18,12 +18,21 @@
 package org.lineageos.settings;
 
 import android.content.BroadcastReceiver;
+import android.content.ContentResolver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.database.ContentObserver;
+import android.hardware.display.DisplayManager;
+import android.os.Handler;
+import android.os.IBinder;
+import android.os.UserHandle;
+import android.provider.Settings;
 import android.util.Log;
 
 import org.lineageos.settings.doze.DozeUtils;
 import org.lineageos.settings.thermal.ThermalUtils;
+import org.lineageos.settings.thermal.ThermalTileService;
 import org.lineageos.settings.refreshrate.RefreshUtils;
 
 public class BootCompletedReceiver extends BroadcastReceiver {
