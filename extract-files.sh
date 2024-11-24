84a75f50e73c106da7833996012a71a0038b9bfe
Arian <arian.kulmer@web.de>
2024-11-24 21:10:34 +0530

    sm8450-common: Allow gettid syscall for wfdhdcphalservice
    10-25 15:22:18.582     1     1 I init    : starting service 'wfdhdcphalservice'...
    10-25 15:22:18.595     1     1 I init    : ... started service 'wfdhdcphalservice' has pid 4702
    10-25 15:22:16.536  4702  4702 E wfdhdcphal: wfdhdcphalservice starting...
    10-25 15:22:16.536  4702  4702 W wfdhdcphalservice: libminijail[4702]: failed to get path of fd 5: No such file or directory
    10-25 15:22:16.536  4702  4702 W wfdhdcphalservice: libminijail[4702]: allowing syscall: connect
    10-25 15:22:16.536  4702  4702 W wfdhdcphalservice: libminijail[4702]: allowing syscall: fcntl
    10-25 15:22:16.536  4702  4702 W wfdhdcphalservice: libminijail[4702]: allowing syscall: sendto
    10-25 15:22:16.536  4702  4702 W wfdhdcphalservice: libminijail[4702]: allowing syscall: socket
    10-25 15:22:16.536  4702  4702 W wfdhdcphalservice: libminijail[4702]: allowing syscall: writev
    10-25 15:22:16.536  4702  4702 W wfdhdcphalservice: libminijail[4702]: compile_file: <fd>(33): nonexistent syscall 'fstatat'
    10-25 15:22:16.536  4702  4702 W wfdhdcphalservice: libminijail[4702]: compile_file: <fd>(52): nonexistent syscall 'open'
    10-25 15:22:16.536  4702  4702 W wfdhdcphalservice: libminijail[4702]: compile_file: <fd>(53): nonexistent syscall 'ugetrlimit'
    10-25 15:22:16.536  4702  4702 W wfdhdcphalservice: libminijail[4702]: compile_file: <fd>(64): nonexistent syscall 'readlink'
    10-25 15:22:16.536  4702  4702 W wfdhdcphalservice: libminijail[4702]: logging seccomp filter failures
    10-25 15:22:16.538  4702  4702 E wfdhdcphalservice: libminijail[4702]: blocked syscall: gettid
    10-25 15:22:18.622     1     1 I init    : Service 'wfdhdcphalservice' (pid 4702) received signal 31
    10-25 15:22:18.623     1     1 I init    : Sending signal 9 to service 'wfdhdcphalservice' (pid 4702) process group...
    10-25 15:22:18.623     1     1 I libprocessgroup: Removed cgroup /sys/fs/cgroup/uid_1000/pid_4702
    10-25 15:22:18.623     1     1 E init    : process with updatable components 'wfdhdcphalservice' exited 4 times in 4 minutes
    10-25 15:22:18.627     1     1 I init    : processing action (sys.init.updatable_crashing=1) from (/system/etc/init/flags_health_check.rc:10)
    10-25 15:22:18.628     1     1 I init    : starting service 'exec 37 (/system/bin/flags_health_check UPDATABLE_CRASHING)'...
    10-25 15:22:18.637     1     1 I init    : ... started service 'exec 37 (/system/bin/flags_health_check UPDATABLE_CRASHING)' has pid 4703
    10-25 15:22:18.637     1     1 I init    : SVC_EXEC service 'exec 37 (/system/bin/flags_health_check UPDATABLE_CRASHING)' pid 4703 (uid 1000 gid 1000+0 context default) started; waiting...
    
    Change-Id: I9ab6ca254a1e6509cfd9de36765496e875407fb0

--- extract-files.sh
+++ extract-files.sh
@@ -101,7 +101,7 @@ function blob_fixup() {
             [ "$2" = "" ] && return 0
             sed -i -E '/media_codecs_(google_audio|google_c2|google_telephony|vendor_audio)/d' "${2}"
             ;;
-        vendor/etc/seccomp_policy/atfwd@2.0.policy)
+        vendor/etc/seccomp_policy/atfwd@2.0.policy | vendor/etc/seccomp_policy/wfdhdcphalservice.policy)
             [ "$2" = "" ] && return 0
             grep -q "gettid: 1" "${2}" || echo "gettid: 1" >> "${2}"
             ;;
