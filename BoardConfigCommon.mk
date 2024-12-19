d017aedf75d2ac43a6dbfaeea69e22faf7930276
Fiqri Ardyansyah <fiqri191002@gmail.com>
2024-12-19 11:29:43 +0700

    sm8450-common: gps: Make sure wearables supports in GPS is disabled
    Signed-off-by: Fiqri Ardyansyah <fiqri191002@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -80,6 +80,7 @@ TARGET_FS_CONFIG_GEN := $(COMMON_PATH)/configs/config.fs
 # GPS
 BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE := default
 $(call soong_config_set, qtilocation, feature_nhz, false)
+$(call soong_config_set, qtilocation, supports_wearables, false)
 
 # Kernel
 BOARD_INCLUDE_DTB_IN_BOOTIMG := true
