e71fa23dfd04fd3c2c42f801c7abfa861abb572b
Joey Huab <jhuab@edukasyon.ph>
2025-01-20 15:54:26 +0900

    Revert "sm8450-common: sensors: Move to new xiaomi-touch"
    This reverts commit 9474c3c7398ddfd56be12a3056bbfaa9d97154c6.

--- sensors/sensor-notifier/notifiers/NonUiNotifier.cpp
+++ sensors/sensor-notifier/notifiers/NonUiNotifier.cpp
@@ -34,11 +34,8 @@ class NonUiSensorCallback : public IEventQueueCallback {
     }
 
     Return<void> onEvent(const Event& e) {
-        struct touch_mode_request request = {
-            .mode = TOUCH_MODE_NONUI_MODE,
-            .value = static_cast<int>(e.u.scalar),
-        };
-        ioctl(touch_fd_.get(), TOUCH_IOC_SET_CUR_VALUE, &request);
+        int buf[MAX_BUF_SIZE] = {0, Touch_Nonui_Mode, static_cast<int>(e.u.scalar)};
+        ioctl(touch_fd_.get(), TOUCH_IOC_SET_CUR_VALUE, &buf);
 
         return Void();
     }
