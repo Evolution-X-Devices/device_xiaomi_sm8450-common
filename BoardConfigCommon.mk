ebf9fad3ae1bb3a62d914950b4e79760b5c9c228
Cosmin Tanislav <demonsingur@gmail.com>
2025-02-19 02:44:05 +0200

    sm8450-common: switch to common QCOM AIDL bootctrl HAL
    Change-Id: I04ab771d3b1c38b58913607fbff1bb3b55e1fe25
    Signed-off-by: Fiqri Ardyansyah <fiqri191002@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -63,9 +63,6 @@ BOARD_SUPPORTS_OPENSOURCE_STHAL := true
 TARGET_PROVIDES_AUDIO_HAL ?= true
 TARGET_USES_QCOM_MM_AUDIO := true
 
-# Boot control
-$(call soong_config_set, ufsbsg, ufsframework, bsg)
-
 # Bootloader
 TARGET_BOOTLOADER_BOARD_NAME := taro
 TARGET_NO_BOOTLOADER := true
