89dffb6039ef8cbba58d574814297271fec847e3
Michael Bestas <mkbestas@gmail.com>
2025-04-29 11:41:39 +0200

    sm8450-common: Move Lineage PowerShare HAL to select()
    Signed-off-by: lemezohaib <zhbkhan644@gmail.com>

--- common.mk
+++ common.mk
@@ -311,6 +311,8 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     vendor/qcom/opensource/power/config/taro/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
 
+$(call soong_config_set,lineage_powershare,powershare_path,/sys/class/qcom-battery/reverse_chg_mode)
+
 # QMI
 PRODUCT_PACKAGES += \
     libvndfwk_detect_jni.qti_vendor:64 # Needed by CNE app
