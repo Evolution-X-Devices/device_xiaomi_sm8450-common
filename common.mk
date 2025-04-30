dc4e92823f47edcd6bf2c3b88b3f1a06c67c5453
Bruno Martins <bgcngm@gmail.com>
2025-04-30 21:12:33 +0100

    sm8450-common: Set camera override format from reserved via soong config
    Change-Id: I6a089e783f26c4515940d74f8e912dffa2789550
    Signed-off-by: Fiqri Ardyansyah <fiqri191002@gmail.com>

--- common.mk
+++ common.mk
@@ -123,6 +123,7 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.camera.full.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.full.xml \
     frameworks/native/data/etc/android.hardware.camera.raw.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.raw.xml
 
+$(call soong_config_set,camera,override_format_from_reserved,true)
 $(call soong_config_set,libcameraservice,ext_lib,libcameraservice_extension.xiaomi_sm8450)
 
 # Display
