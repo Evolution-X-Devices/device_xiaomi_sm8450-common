0c5005216cb51e26da7c64c0984e04d2a38acfc9
swiitchOFF <120115258+swiitchOFF@users.noreply.github.com>
2025-05-18 22:55:17 +0530

    sm8450-common: Add support for Dolby Vision
    * Also import Media codecs for Dolby Vision support
    * Fixes taken from multiple trees which mentioned below
    * Let's make it easy and clean adaption in a single commit xD
    * Thanks to the authors of:
    - https://github.com/flakeforever/device_xiaomi_mondrian
    - https://github.com/Nabu-upsidedowncake/device_xiaomi_marble
    - https://github.com/AOSPA/android_device_xiaomi_sm8450-common
    - https://github.com/AOSPA/android_device_xiaomi_sm8550-common
    - https://github.com/blazerpaul15
    
    Signed-off-by: swiitchOFF <120115258+swiitchOFF@users.noreply.github.com>

--- extract-files.py
+++ extract-files.py
@@ -104,10 +104,14 @@ blob_fixups: blob_fixups_user_type = {
         .regex_replace(r'(?s)(<MediaCodecs.*?>)',r'\1\n    <Include href="media_codecs_dolby_audio.xml" />'),
     'vendor/etc/seccomp_policy/c2audio.vendor.ext-arm64.policy': blob_fixup()
         .add_line_if_missing('setsockopt: 1'),
-    'vendor/etc/vintf/manifest/c2_manifest_vendor.xml': blob_fixup()
-        .regex_replace('.+dolby.+\n', ''),
     'vendor/lib64/vendor.libdpmframework.so': blob_fixup()
         .add_needed('libhidlbase_shim.so'),
+        ('vendor/lib/c2.dolby.avc.dec.so', 'vendor/lib/c2.dolby.avc.sec.dec.so', 'vendor/lib/c2.dolby.hevc.dec.so', 'vendor/lib/c2.dolby.hevc.sec.dec.so'): blob_fixup()
+        .add_needed('libstagefright_foundation-v33.so'),
+        ('vendor/bin/hw/dolbycodec2'): blob_fixup()
+        .add_needed('libstagefright_foundation-v33.so'),
+        ('vendor/lib/c2.dolby.client.so'): blob_fixup()
+        .add_needed('libcodec2_hidl_shim.so'),
 }  # fmt: skip
 
 module = ExtractUtilsModule(
