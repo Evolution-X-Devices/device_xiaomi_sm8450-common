9bf89c8c9bfaad49210d04f86d9efb7fc5f66996
Dmitry <42121541+dkpost3@users.noreply.github.com>
2025-05-19 12:32:37 +0300

    Update BoardConfigCommon.mk

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -206,21 +206,23 @@ SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/public
 BOARD_VENDOR_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/vendor
 
 # VINTF
-DEVICE_MATRIX_FILE += hardware/qcom-caf/common/compatibility_matrix.xml
- 
-DEVICE_MANIFEST_FILE += \
-    $(COMMON_PATH)/vintf/manifest.xml \
-    $(COMMON_PATH)/vintf/manifest_xiaomi.xml
- 
-ifeq ($(TARGET_NFC_SUPPORTED_SKUS),)
-DEVICE_MANIFEST_FILE += \
-    $(COMMON_PATH)/vintf/manifest_no_nfc.xml
-else
-ODM_MANIFEST_FILES += \
-    $(COMMON_PATH)/vintf/manifest_nfc.xml
+DEVICE_MATRIX_FILE := hardware/qcom-caf/common/compatibility_matrix.xml
+
+DEVICE_MANIFEST_SKUS := taro diwali cape ukee
+$(foreach sku, $(call to-upper, $(DEVICE_MANIFEST_SKUS)), \
+    $(eval DEVICE_MANIFEST_$(sku)_FILES := \
+        $(COMMON_PATH)/vintf/manifest.xml \
+        $(COMMON_PATH)/vintf/manifest_xiaomi.xml \
+        $(if $(TARGET_NFC_SUPPORTED_SKUS),$(COMMON_PATH)/vintf/manifest_no_nfc.xml,) \
+    ))
+
+ifneq ($(TARGET_NFC_SUPPORTED_SKUS),)
+ODM_MANIFEST_SKUS += $(TARGET_NFC_SUPPORTED_SKUS)
+$(foreach nfc_sku, $(call to-upper, $(TARGET_NFC_SUPPORTED_SKUS)), \
+    $(eval ODM_MANIFEST_$(nfc_sku)_FILES += $(COMMON_PATH)/vintf/manifest_nfc.xml))
 endif
 
-DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
     hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
     hardware/xiaomi/vintf/xiaomi_framework_compatibility_matrix.xml \
     vendor/lineage/config/device_framework_matrix.xml
