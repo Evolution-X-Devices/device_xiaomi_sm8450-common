be273659d0a652ae0d382d393e2e4e5da9a0891a
Fiqri Ardyansyah <fiqri191002@gmail.com>
2025-09-17 19:50:36 +0900

    sm8450-common: Drop unused QTI VM manager blobs
    marble did not shipping vm-bootsys firmware image.
    
    Change-Id: I6d7cb43fe5bffc1989e882df70196fa7d6e44413
    Signed-off-by: Fiqri Ardyansyah <fiqri191002@gmail.com>

--- common.mk
+++ common.mk
@@ -322,8 +322,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     vendor_bt_firmware_mountpoint \
     vendor_dsp_mountpoint \
-    vendor_firmware_mnt_mountpoint \
-    vendor_vm-system_mountpoint
+    vendor_firmware_mnt_mountpoint
 
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
