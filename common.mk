a8f1602fd4e6c23aa39bb746d60938f7f42b72c7
Luffitys <Luffitys@users.noreply.github.com>
2025-09-19 05:33:31 +0000

    sm8450-common: Build missing wifi-display libs
    Co-Authored-By: EdwinMoq <EdwinMoq@users.noreply.github.com>

--- common.mk
+++ common.mk
@@ -507,6 +507,11 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.wifi.rtt.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.wifi.rtt.xml \
     frameworks/native/data/etc/android.hardware.wifi.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.wifi.xml
 
+# WiFi Display
+PRODUCT_PACKAGES += \
+    android.media.audio.common.types-V2-cpp:64 \
+    vendor.qti.hardware.display.config-V5-ndk:64
+
 # WiFi firmware symlinks
 PRODUCT_PACKAGES += \
     firmware_WCNSS_qcom_cfg.ini_symlink \
