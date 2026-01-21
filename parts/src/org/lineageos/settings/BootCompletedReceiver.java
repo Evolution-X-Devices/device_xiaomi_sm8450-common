eb7c73e81b7ccf72892255aa4df831af00aec408
zenzer0s <zenzer0s@users.noreply.github.com>
2026-01-21 13:33:43 +0000

    sm8450-common: parts: thermals: Re-write in jetpack compose
    Signed-off-by: pabloescobar-reborn <pabloescobarreborn77@gmail.com>

--- parts/src/org/lineageos/settings/BootCompletedReceiver.java
+++ parts/src/org/lineageos/settings/BootCompletedReceiver.java
@@ -78,7 +78,7 @@ public class BootCompletedReceiver extends BroadcastReceiver {
         DozeUtils.onBootCompleted(context);
 
         // Start Thermal Management Services
-        ThermalUtils.Companion.getInstance(context).startService();
+        ThermalUtils.getInstance(context).startService();
     }
 
     private void overrideHdrTypes(Context context) {
