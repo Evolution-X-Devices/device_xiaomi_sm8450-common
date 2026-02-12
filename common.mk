4062f545c9cf6c7bb98ec46af5a846fa47f51c4e
Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>
2026-02-13 01:04:36 +0600

    sm8450-common: Set target min supported frame rate of 60
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- common.mk
+++ common.mk
@@ -137,6 +137,7 @@ PRODUCT_PACKAGES += \
 
 # Display
 $(call soong_config_set,surfaceflinger,frame_rate_category_high,120)
+$(call soong_config_set,surfaceflinger,frame_rate_category_min,60)
 
 PRODUCT_PACKAGES += \
     android.hardware.graphics.mapper@4.0-impl-qti-display \
