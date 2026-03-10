884fde0652ef42b3c13de0d38fc2f02d4f8728ef
bgcngm <bgcngm@users.noreply.github.com>
2026-03-10 21:43:44 +0530

    sm8450-common: Migrate to common libqti-perfd-client and power-libperfmgr
    -Import powerhint.json from lahaina
    -powerhint: Adapt and tune for marble
    
    Co-authored-by: beingashwani <ashwanic177@gmail.com>
    Co-authored-by: bheatleyyy <bheatleyyy@users.noreply.github.com>

--- common.mk
+++ common.mk
@@ -336,10 +336,11 @@ PRODUCT_COPY_FILES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power-service-qti
+    android.hardware.power-service.lineage-libperfmgr \
+    libqti-perfd-client
 
 PRODUCT_COPY_FILES += \
-    vendor/qcom/opensource/power/config/taro/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
+    $(LOCAL_PATH)/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
 
 # Powershare
 $(call soong_config_set,lineage_powershare,powershare_path,/sys/class/qcom-battery/reverse_chg_mode)
@@ -378,6 +379,10 @@ $(foreach sku, taro diwali cape ukee, \
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
+    hardware/google/interfaces \
+    hardware/google/pixel \
+    hardware/lineage/interfaces/power-libperfmgr \
+    hardware/qcom-caf/common/libqti-perfd-client \
     hardware/xiaomi \
     vendor/qcom/opensource/usb/etc
 
