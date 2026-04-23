b0a751838bea5262fd899a7ad1f0c047877cccf1
Joey <joey@evolution-x.org>
2026-04-23 14:52:23 +0900

    Revert "sm8450-common: Migrate to common libqti-perfd-client and power-libperfmgr"
    This reverts commit 2fca46dbb39a8512e53278e51acf999cf02ddc08.
    
    Revert "sm8450-common: sync powerhint"
    
    This reverts commit cb08032a2f579dac5a0377bb65399c2632fdb117.

--- common.mk
+++ common.mk
@@ -312,11 +312,10 @@ PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power-service.lineage-libperfmgr \
-    libqti-perfd-client
+    android.hardware.power-service-qti
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
+    vendor/qcom/opensource/power/config/taro/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
 
 # Powershare
 $(call soong_config_set,lineage_powershare,powershare_path,/sys/class/qcom-battery/reverse_chg_mode)
@@ -355,10 +354,6 @@ $(foreach sku, taro diwali cape ukee, \
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
-    hardware/google/interfaces \
-    hardware/google/pixel \
-    hardware/lineage/interfaces/power-libperfmgr \
-    hardware/qcom-caf/common/libqti-perfd-client \
     hardware/xiaomi \
     vendor/qcom/opensource/usb/etc
 
