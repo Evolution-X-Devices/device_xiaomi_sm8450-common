8d761b770a008ded8fd0f087ccb80ce0dde0d3f8
beingashwani <ashwanic177@gmail.com>
2026-05-17 19:38:54 +0530

    sm8450-common: import cgroup.json from aospa
    Here: https://github.com/AOSPA/android_system_core/blob/beryl/libprocessgroup/profiles/cgroups.json

--- common.mk
+++ common.mk
@@ -384,7 +384,8 @@ PRODUCT_SOONG_NAMESPACES += \
 
 # task profile
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/task_profiles.json:$(TARGET_COPY_OUT_VENDOR)/etc/task_profiles.json
+    $(LOCAL_PATH)/configs/task_profiles.json:$(TARGET_COPY_OUT_VENDOR)/etc/task_profiles.json \
+    $(LOCAL_PATH)/configs/cgroups.json:$(TARGET_COPY_OUT_VENDOR)/etc/cgroups.json
 
 # Telephony
 PRODUCT_PACKAGES += \
