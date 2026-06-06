ec378f50914488dbc87e8f279c9762fbefd2e89e
pabloescobar-reborn <yogivangala07@gmail.com>
2026-06-06 06:04:57 +0000

    sm8450-common: bring back dolby vision
    restore Dolby Codec2 VINTF entries
    
    Change-Id: Ica3d14be57f1e22a7a084dca021fc2fcaa16c26b
    Signed-off-by: pabloescobar-reborn <yogivangala07@gmail.com>

--- extract-files.py
+++ extract-files.py
@@ -90,8 +90,6 @@ blob_fixups: blob_fixups_user_type = {
     ): blob_fixup()
         .regex_replace('.+media_codecs_(google_audio|google_c2|google_telephony|vendor_audio).+\n', '')
         .regex_replace(r'(?s)(<MediaCodecs.*?>)',r'\1\n    <Include href="media_codecs_dolby_audio.xml" />'),
-    'vendor/etc/vintf/manifest/c2_manifest_vendor.xml': blob_fixup()
-        .regex_replace('.+dolby.+\n', ''),
     (
         'vendor/lib64/libdpps.so',
         'vendor/lib64/libsnapdragoncolor-manager.so',
